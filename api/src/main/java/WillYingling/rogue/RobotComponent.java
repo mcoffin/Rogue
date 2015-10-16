64a75e43fe8bd39e3fd97bbc3158118c8d1fc924
Will Yingling <willyingling@gmail.com>
2015-10-16 11:59:05 -0600

    Removed Drive, it wasn't doing much

--- api/src/main/java/WillYingling/rogue/RobotComponent.java
+++ api/src/main/java/WillYingling/rogue/RobotComponent.java
@@ -1,12 +1,16 @@
-public interface RobotComponent {
+public abstract class RobotComponent {
+
+	public RobotComponent() {
+		init();
+	}
 
 	// This method should create all the resources needed for the component
-	public boolean init();
+	abstract boolean init();
 
 	// This method should free all resources used, like Digital IO, Servo's, etc.
-	public boolean halt();
+	abstract boolean halt();
 
 	// This method should be called repeatedly over the course of the components lifetime.
 	// Any manipulation of the robot should happen here.
-	public void update();
+	abstract void update();
 }
