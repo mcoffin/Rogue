90fc1f59286380a26ed10b2acb43d9506db343a5
Will Yingling <willyingling@gmail.com>
2015-10-23 21:26:16 -0600

    Reorganized tests, removed an outdated test, added WpiActivatorTest

---
+++ wpi/src/test/java/mcoffin/rogue/wpi/WpiActivatorTest.java
@@ -0,0 +1,14 @@
+package mcoffin.rogue.wpi;
+
+public class WpiActivatorTest {
+
+	 
+	@Test (expected = RuntimeException.class)
+	public void prestartShouldFail() {
+		Object runner = new Object();
+
+		WpiActivator wa = new WpiActivator(runner);
+		wa.prestart();
+	}
+
+}
