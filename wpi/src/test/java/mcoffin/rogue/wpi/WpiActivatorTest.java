0b2ff9b0a58cced004ba47cfc24f86fae4bf26d4
Will Yingling <willyingling@gmail.com>
2015-10-23 22:06:34 -0600

    Fixed indentation

--- wpi/src/test/java/mcoffin/rogue/wpi/WpiActivatorTest.java
+++ wpi/src/test/java/mcoffin/rogue/wpi/WpiActivatorTest.java
@@ -1,14 +1,13 @@
 package mcoffin.rogue.wpi;
 
 public class WpiActivatorTest {
+   
+  @Test (expected = RuntimeException.class)
+  public void prestartShouldFail() {
+    Object runner = new Object();
 
-	 
-	@Test (expected = RuntimeException.class)
-	public void prestartShouldFail() {
-		Object runner = new Object();
-
-		WpiActivator wa = new WpiActivator(runner);
-		wa.prestart();
-	}
+    WpiActivator wa = new WpiActivator(runner);
+    wa.prestart();
+  }
 
 }
